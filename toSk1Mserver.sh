d5a2f27dcdc501358438f83beaa5af723aa4099c
Edgar Toll <edjopato@gmail.com>
2018-02-05 12:54:32 +0100

    chore: only upload when hugo is successful

--- toSk1Mserver.sh
+++ toSk1Mserver.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 rm -rf public
-hugo
-rsync -acv --delete-delay public/ sk1m.de:/usr/share/nginx/de.edjopato/ | grep -E '^deleting|[^/]$|^$'
+hugo && rsync -acv --delete-delay public/ sk1m.de:/usr/share/nginx/de.edjopato/ | grep -E '^deleting|[^/]$|^$'

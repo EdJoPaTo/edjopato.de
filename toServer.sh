c4fa6cee5bb0ab3cb63aa62f4bf1812e8a692366
EdJoPaTo <edjopato@gmail.com>
2019-12-03 16:36:43 +0100

    chore(toServer): fix strange transmission bug
    maybe its macOS...

--- toServer.sh
+++ toServer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -rf public
-hugo && rsync -acv --exclude=.DS_Store --delete-delay public/ www.3t0.de:/var/www/edjopato.de/ | grep -E '^deleting|[^/]$|^$'
+hugo && rsync -acv --omit-dir-times --exclude=.DS_Store --delete-delay public/ www.3t0.de:/var/www/edjopato.de/ | grep -E '^deleting|[^/]$|^$'

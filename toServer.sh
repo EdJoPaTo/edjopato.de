eb1674ffecb0d759597ad7cb2fa91c9eea0f40be
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-05-02 14:42:00 +0200

    chore(toServer): ignore tags and categories in output

--- toServer.sh
+++ toServer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -rf public
-hugo && rsync -acv --omit-dir-times --exclude=.DS_Store --delete-delay public/ www.3t0.de:/var/www/edjopato.de/ | grep -E '^deleting|[^/]$|^$' | grep -vE '/page/'
+hugo && rsync -acv --omit-dir-times --exclude=.DS_Store --delete-delay public/ www.3t0.de:/var/www/edjopato.de/ | grep -E '^deleting|[^/]$|^$' | grep -v '/page/' | grep -v '^tags/' | grep -v '^categories/'

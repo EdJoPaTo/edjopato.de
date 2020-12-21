57f049cca7d6007022eb36d3ab737c6aef9ecb4b
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-12-21 01:42:19 +0100

    chore(toServer): use set -e

--- toServer.sh
+++ toServer.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+set -e
+
 rm -rf public
-hugo && rsync -acv --omit-dir-times --exclude=.DS_Store --delete-delay public/ www.edjopato.de:/var/www/edjopato.de/ | grep -E '^deleting|[^/]$|^$' | grep -v '/page/' | grep -v '^tags/' | grep -v '^categories/'
+hugo
+rsync -acv --omit-dir-times --exclude=.DS_Store --delete-delay public/ www.edjopato.de:/var/www/edjopato.de/ | grep -E '^deleting|[^/]$|^$' | grep -v '/page/' | grep -v '^tags/' | grep -v '^categories/'

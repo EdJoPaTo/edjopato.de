9762b69834e7c0c0d04737ab1b4bffcaf715f356
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-07-08 14:44:04 +0200

    feat(theme): use time hue as link color

--- themes/edjopato-hugo-theme/assets/js/updatebg.js
+++ themes/edjopato-hugo-theme/assets/js/updatebg.js
@@ -5,8 +5,7 @@ function updateBg() {
 	const minuteOfDay = (hour * 60) + minute;
 	const hue = minuteOfDay % 360;
 
-	const element = document.getElementsByTagName("html")[0];
-	element.style["background-color"] = "hsl(" + hue + ",100%,25%)";
+	document.documentElement.style.setProperty('--time-hue', hue);
 	console.log("update bg hue", hue, date);
 }
 setInterval(updateBg, 30000);

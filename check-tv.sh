299a705e2c2d5252eb0e5431a1dd234e683a7384
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-10-03 18:28:21 +0200

    chore(tv): add check-tv script

---
+++ check-tv.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+urls=$(grep "https://" static/tv.m3u)
+
+for url in $urls; do
+    echo "::group::$url"
+
+    nice ffmpeg -y -v error -i "$url" -c copy -codec:v h264 -t 0:05 -f mp4 /dev/null
+
+    echo "::endgroup::"
+done

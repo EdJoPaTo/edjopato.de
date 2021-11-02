cfca60e8c7e12f0a5584d3aef3cc026b8093ab73
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-11-02 18:00:44 +0100

    chore(toServer): add --delay-updates

--- toServer.sh
+++ toServer.sh
@@ -3,7 +3,9 @@ set -e
 
 rm -rf public
 hugo
-rsync -acv --compress --omit-dir-times --exclude=.DS_Store --delete-delay public/ www.edjopato.de:/var/www/edjopato.de/ \
+rsync -acv --compress --omit-dir-times --delete-delay --delay-updates \
+    --exclude=.DS_Store \
+    public/ www.edjopato.de:/var/www/edjopato.de/ \
     | grep -v '/page/' \
     | grep -v '^tags/' \
     | grep -v '^categories/'

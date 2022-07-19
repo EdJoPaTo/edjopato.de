176f1fa98ea6328ce8629d3a028aa9c07a678dfd
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2022-07-19 18:22:13 +0200

    chore(toServer): omit-dir-times

--- toServer.sh
+++ toServer.sh
@@ -4,11 +4,10 @@ set -e
 rm -rf public
 hugo
 rsync \
-	--recursive --perms --times \
+	--recursive --perms --times --omit-dir-times \
 	--compress --verbose --checksum --delete-delay --delay-updates \
 	--exclude=.DS_Store \
 	public/ www.edjopato.de:/var/www/edjopato.de/ \
 	| grep -v '/page/' \
 	| grep -v '^tags/' \
-	| grep -v '^categories/' \
-	| grep -v '/$'
+	| grep -v '^categories/'
